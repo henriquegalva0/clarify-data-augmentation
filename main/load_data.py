b1194fb38bf41f75b9e951fd885d7c8c30a3c253
Henrique Galvão <henriquegalvao@discente.ufg.br>
2025-10-16 17:13:50 -0300

    Remove unused target_label variable
    Removed unused target_label variable.

--- main/load_data.py
+++ main/load_data.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from datasets import load_dataset
 
-target_label="Partial/half-answer"
-
 dataset = load_dataset("ailsntua/QEvasion", split='train')
 
 full_dataset = dataset.to_pandas()
