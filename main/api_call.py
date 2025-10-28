5468ebbd98db2143a8a48e30c9176d5950e34e85
henriquegalva0 <henriquegalvao@discente.ufg.br>
2025-10-28 12:25:29 -0300

    Multiple prompts, APIs & fixing issues

--- main/api_call.py
+++ main/api_call.py
@@ -7,6 +7,14 @@ import json
 
 from load_data import parsed_dataset
 
+google_target_api_key=0
+api_keys=[
+    "CHANGEME1",
+    "CHANGEME2",
+    "CHANGEME3",
+    "CHANGEME4"
+    ]
+
 def target_sample(fewshot_dataset): 
     max_length=fewshot_dataset["evasion_label"].count()
     num=randint(0,max_length-1)
@@ -14,25 +22,37 @@ def target_sample(fewshot_dataset):
     
     return str("{{" + f'"interview_question": "{row_data["interview_question"]}", "interview_answer": "{row_data["interview_answer"]}", "evasion_label": "{row_data["evasion_label"]}"' + "}}")
 
-with open("clarify-data-augmentation/main/prompt.txt","r",encoding="utf-8") as prompt_txt:
-    template_prompt = prompt_txt.read()
-
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
-    temperature=0.45,
-    google_api_key="CHANGEME"
-)
-
-template_text = (template_prompt)
-prompt = PromptTemplate(
-    input_variables=["sample1", "sample2", "sample3", "sample4", "sample5"],
-    template=template_text
-)
-
-qa_chain = prompt | llm | StrOutputParser()
-
 def generate_data(target_label):
-    
+
+    filename = target_label.lower().replace(' ', '').replace('/', '') + ".txt"
+    path = f"clarify-data-augmentation/main/prompts/{filename}"
+
+    with open(path, "r", encoding="utf-8") as prompt_txt:
+        template_prompt = prompt_txt.read()
+
+    print("Accessing the Path:",path)
+
+    while True:
+        try:
+            llm = ChatGoogleGenerativeAI(
+                model="gemini-2.0-flash",
+                temperature=0.45,
+                google_api_key=api_keys[google_target_api_key]
+            )
+            break
+        except:
+            google_target_api_key+=1
+
+    print("Using API Key:",api_keys[google_target_api_key])
+
+    template_text = (template_prompt)
+    prompt = PromptTemplate(
+        input_variables=["sample1", "sample2", "sample3", "sample4", "sample5"],
+        template=template_text
+    )
+
+    qa_chain = prompt | llm | StrOutputParser()
+
     fewshot_dataset = parsed_dataset[parsed_dataset["evasion_label"] == target_label]
     fewshot_dataset.reset_index(drop=True, inplace=True)
     
--- main/load_data.py
+++ main/load_data.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from datasets import load_dataset
 
@@ -6,4 +7,8 @@ dataset = load_dataset("ailsntua/QEvasion", split='train')
 full_dataset = dataset.to_pandas()
 
 parsed_dataset = full_dataset[["interview_question", "interview_answer", "evasion_label"]]
-parsed_dataset.to_csv("augmented_dataset.csv",index=False)
+
+if os.path.exists("clarify-data-augmentation/augmented_dataset.csv"):
+    print("Augmented dataset already exists. Loading from 'augmented_dataset.csv'.")
+else:
+    parsed_dataset.to_csv("augmented_dataset.csv",index=False)
